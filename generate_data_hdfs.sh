062261c64f17c1d7c72ccc6cfdb0e3824e1746ce
BalaS <balashukla0761@gmail.com>
2025-11-08 20:18:43 -0600

    Add data generation workaround and fix Java dependencies

--- generate_data_hdfs.sh
+++ generate_data_hdfs.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# Quick script to generate test data from HDFS container
+# Helper script to generate test data directly from HDFS container
 
-# Copy the test data generator to HDFS container
-docker cp create_test_data.py mapreduce-hdfs-1:/tmp/
-
-# Install required Python packages in HDFS container
-docker-compose exec hdfs pip3 install pandas pyarrow numpy
-
-# Generate the test data
-docker-compose exec hdfs python3 /tmp/create_test_data.py --size "$@"
-
+docker-compose exec hdfs bash -c "python3 /home/hdfs_user/create_test_data.py --size $1"
